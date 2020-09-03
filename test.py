82c450bd1816a1a50da717bd89048091efb6880c
aiegoo <eozz21@gmail.com>
2020-09-03 15:49:56 +0900

    add exam0 index11 files as sample

---
+++ test.py
@@ -0,0 +1,10 @@
+x=int(input("Insert a value:"));
+print(x);
+
+if x<0:
+     print(f'minus {x}');
+age = int(input("Insert your age: "));
+if age>=20:
+     print('adult');
+else:
+     print(f'you will be an adult {x} years');
